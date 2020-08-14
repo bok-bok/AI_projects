f11e19a5a2ba8b9fab2a4634f2bc84c092ca9936
Kyungbok Lee <dlrudqhr7180@gmail.com>
2020-08-14 21:12:30 +0900

    made part of board

--- tic_tac_toe_AI/game.py
+++ tic_tac_toe_AI/game.py
@@ -1,85 +1,80 @@
-import copy
-new_board = [[" "," "," "],
-             [" "," "," "],
-             [" "," "," "]]
-X = "X"
-O = "O"
+import pickle
+import numpy as np
 
-def player(board):
-    x = 0
-    o = 0
-    for r in board:
-        for c in r:
-            if c == X:
-                x += 1
-            elif c == O:
-                o += 1
+ROW = 3
+COL = 3
 
-    if x > o:
-        return O
-    else:
-        return X
+class State:
+    def __init__(self,p1,p2):
+        self.board = np.zeros(ROW,COL)
+        self.p1 = p1
+        self.p2 = p2
+        self.end = False
+        self.boardHash = None
 
-def print_board(board):
-    print("-------------")
-    print("| "+board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |")
-    print("-------------")
-    print("| "+board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " | ")
-    print("-------------")
-    print("| "+board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " | ")
-    print("-------------")
-def actions(board):
-    possible_actions = set()
-    for i, r in enumerate(board):
-        for j, c in enumerate(r):
-            if c == " ":
-                possible_actions.add(i,j)
-    return possible_actions
-
-def result(board,action):
-    nBoard = copy.deepcopy(board)
-    p = player(board)
-    nBoard[action[0]][action[1]] = p
-    return nBoard
+        self.playerSymbol = 1
 
 
-def winner(board):
-    win = [[(0,0),(0,1),(0,2)],
-           [(1,0),(1,1),(1,2)],
-           [(2,0),(2,1),(2,2)],
-           [(0,0),(1,0),(2,0)],
-           [(0,1),(1,1),(2,1)],
-           [(0,2),(1,2),(2,2)],
-           [(0,0),(1,1),(2,2)],
-           [(0,2),(1,1),(2,0)]]
-    for combination in win:
-        x = 0
-        o = 0
-        for r, c in combination:
-            if board[r][c] == X:
-                x+=1
-            elif board[r][c] == O:
-                o += 1
-        if x == 3:
-            return X
-        elif o == 3:
-            return O
-    return None
+    # get unique hash of current board state
+    def getHash(self):
+        self.boardHash = str(self.board.reshape(COL*ROW))
+        return self.boardHash
 
-def terminal(board):
-    if winner(board) is not None or not actions(board):
-        return True
-    else:
-        return False
 
-def utility(board):
-    if winner(board) is X:
-        return 1
-    elif winner(board) is O:
-        return -1
-    else:
-        return 0
+    def winner(self):
+        win = [[(0, 0), (0, 1), (0, 2)],
+               [(1, 0), (1, 1), (1, 2)],
+               [(2, 0), (2, 1), (2, 2)],
+               [(0, 0), (1, 0), (2, 0)],
+               [(0, 1), (1, 1), (2, 1)],
+               [(0, 2), (1, 2), (2, 2)],
+               [(0, 0), (1, 1), (2, 2)],
+               [(0, 2), (1, 1), (2, 0)]]
+        for combination in win:
+            play1 = 0
+            play2 = 0
+            for r, c in combination:
+                if self.board[r][c] == 1:
+                    play1 += 1
+                elif self.board[r][c] == 0:
+                    play2 += 1
+            if play1 == 3:
+                return -1
+            elif play2 == 3:
+                return 1
+        return None
 
-def minimax(board):
-    if(terminal(board)):
-        return utility(board)
+
+
+    def avaiablePosition(self):
+        position = []
+        for i in range(ROW):
+            for j in range(COL):
+                if self.board[i][j] == 0:
+                    position.append((i,j))
+
+        return position
+
+
+
+    def updateState(self,position):
+        self.board[position] = self.playerSymbol
+        self.playerSymbol = 1 if self.playerSymbol == 1 else -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
